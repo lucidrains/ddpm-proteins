2bac27acf648650f76774c20f313c9e469fa1a0e
Phil Wang <lucidrains@gmail.com>
2021-06-14 13:10:20 -0700

    0.0.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'ddpm-proteins',
   packages = find_packages(),
-  version = '0.0.4',
+  version = '0.0.5',
   license='MIT',
   description = 'Denoising Diffusion Probabilistic Models - for Proteins - Pytorch',
   author = 'Phil Wang',
